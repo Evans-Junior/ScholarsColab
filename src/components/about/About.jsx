83a49e84ac7429a3f619a81e86b16cf9f7b261e8
Evans-Junior <kwakukumi14@gmail.com>
2023-03-26 00:11:24 +0000

    first commit

---
+++ src/components/about/About.jsx
@@ -0,0 +1,18 @@
+import React from "react"
+import "./about.css"
+import Back from "../common/back/Back"
+import AboutCard from "./AboutCard"
+import AboutFirst from "./AboutFirst"
+import Home from "./Home"
+
+const About = () => {
+  return (
+    <>
+    <Home/>
+    <AboutFirst/>
+    
+    </>
+  )
+}
+
+export default About
